95af859bcb902d2b3806355a1ca4177d9dcbc63e
isac-pedroso <isacsouzapedroso@gmail.com>
2025-04-02 22:41:11 -0300

    Atualizações de rotas

--- primeiroprojeto.api/routes/web.php
+++ primeiroprojeto.api/routes/web.php
@@ -16,6 +16,49 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/teste', function(){
-    return view('teste');
+//Cria rota com view
+/*Route::get('/teste', function(){
+    return view('site/teste');
+});*/
+// Cria rota mais simples com view
+Route::view('/teste', 'site/teste');
+// Cria rota para permitindo apenas os acessos defenidos
+Route::match(['get', 'post'], '/match',function(){
+    return "Permite apenas acessos defenidos !";
+});
+// Cria rota passando parametro
+Route::get('/produto/{id}', function($id){
+    return "Id do produto é ".$id;
+});
+// Cria rota passando parametros que não precisam ser obrigatorios
+Route::get('/testeParam/{id?}/{cat?}', function($id = '', $cat = ''){
+    return "Teste".$id." ".$cat;
+});
+
+// Redirecionamentos
+// Redirecionamento mais complexo com muito codigo
+Route::get('/sobre', function(){
+    return redirect('/empresa');
+});
+// Redirecionamento simples 
+Route::redirect('/testeRedirect', 'http://localhost/projetos-laravel/primeiroprojeto.api/public/teste');
+// Criação de rotas nomeadas
+Route::get('/new', function(){
+    return view('site/news');
+})->name('noticias');
+Route::get('/novidades', function(){
+    return redirect()->route('noticias');
+});
+
+// Grupo de rotas
+Route::prefix('admin')->group(function(){
+    Route::get('dashboard', function(){
+        return 'dashboard';
+    });
+    Route::get('usuarios', function(){
+        return 'usuarios';
+    });
+    Route::get('clientes', function(){
+        return 'clientes';
+    });
 });
